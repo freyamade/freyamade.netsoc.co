da071f1c7f5ffb475adad2e6b6ef2ebd6bac69f5
Ciaran Broderick <cb@cix.ie>
2019-01-16 11:15:35 +0000

    Moved style to a separate stylesheet

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import cgi
 from cgitb import enable
-enable()  # Traceback. Comment out in production
+# enable()  # Traceback. Comment out in production
 
 import os
 
@@ -16,85 +16,8 @@ print("""
     <head>
         <title>crnlpanic - Techtalks and other stuff maybe</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
-        <style>
-            html, body {
-                height: 100%%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%%;
-                color: #d4def6;
-                background-color: #002366;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 96px;
-                margin-bottom: 40px;
-                margin-top: 40px;
-            }
-
-            .quote {
-                font-size: 24px;
-            }
-
-            .cards {
-                display: flex;
-                flex-direction: column;
-                justify-content: space-evenly;
-            }
-
-            .card {
-                flex-grow: 1;
-                margin: 1%% 0;
-                z-index: 1;
-                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-                background-color: #00256e;
-                text-decoration: none;
-                color: #d4def6;
-            }
-
-            .card:hover {
-                background-color: #002876;
-                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.38);
-            }
-
-            .card img {
-                width: 100%%;
-                height: auto;
-            }
-
-            @media only screen and (min-width: 768px) {
-                /* For mobile */
-                .cards {
-                    flex-direction: row;
-                }
-
-                .title {
-                    margin-top: 0;
-                }
-
-                .card {
-                    margin: 0 1%%;
-                }
-            }
-        </style>
+        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="style.css" type="text/css" rel="stylesheet" />
         <meta property="og:title" content="crnlpanic" />
         <meta property="og:url" content="http://%(http_host)s%(request_uri)s" />
         <meta property="og:image" content="http://netsoc.co/rk/wp-content/uploads/2015/10/generic-og.png" />
